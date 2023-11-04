f2021400b4d1dd29a97b701ae57ed28bfcef48f3
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2023-11-04 10:57:05 -0400

    Fixed more styling, added calendar

---
+++ src/components/Widgets/Calendar/Calendar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Calendar() {
+    return (
+
+        <div className="centered-text">
+       
+        <h1>Notes</h1>
+        </div>
+    )
+}
+
+export default Calendar;
