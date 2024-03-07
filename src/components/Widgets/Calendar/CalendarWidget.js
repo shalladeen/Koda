a7b162c2f3ac9e30d21bb863a87e22777aadc73c
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2024-03-06 22:46:50 -0500

    Fixed event handling after converting to using FullCalendar

--- src/components/Widgets/Calendar/CalendarWidget.js
+++ src/components/Widgets/Calendar/CalendarWidget.js
@@ -1,157 +1,167 @@
 import React, { useState, useEffect } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import '../Calendar/CalendarStyle.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { useColorMode, Box, Text, VStack, Button, Modal, ModalOverlay, useDisclosure, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Checkbox, Flex, ChakraProvider } from '@chakra-ui/react';
 import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import {Box,Button,Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,
-  useDisclosure,Input,FormControl,FormLabel,ChakraProvider,VStack,Text,Checkbox, Flex} from '@chakra-ui/react';
-import { SingleDatepicker } from 'chakra-dayzed-datepicker';
+import '@fullcalendar/react';
 
-const localizer = momentLocalizer(moment);
-
-const MyCalendarWidget = () => {
-  const [events, setEvents] = useState(() => {
-    const savedEvents = localStorage.getItem('events');
-    return savedEvents ? JSON.parse(savedEvents) : [];
-  });
+const MyFullCalendar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
   const [currentEvent, setCurrentEvent] = useState(null);
   const [eventTitle, setEventTitle] = useState('');
   const [allDay, setAllDay] = useState(false);
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [startTime, setStartTime] = useState('09:00');
+  const [endTime, setEndTime] = useState('17:00');
+  const { colorMode } = useColorMode();
 
   useEffect(() => {
-    localStorage.setItem('events', JSON.stringify(events));
+    const savedEvents = localStorage.getItem('fullCalendarEvents');
+    setEvents(savedEvents ? JSON.parse(savedEvents) : []);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('fullCalendarEvents', JSON.stringify(events));
   }, [events]);
 
-  const handleSelectSlot = (slotInfo) => {
-    onOpen();
-    setSelectedDate(slotInfo.start);
+  const handleDateSelect = (selectInfo) => {
+    setSelectedDate(selectInfo.start);
+    setAllDay(selectInfo.allDay);
     setCurrentEvent(null);
     setEventTitle('');
-    setAllDay(false);
-    setStartTime('');
-    setEndTime('');
+    onOpen();
   };
 
-  const handleSelectEvent = (event) => {
+  const handleEventClick = (clickInfo) => {
+    setCurrentEvent({
+        id: clickInfo.event.id,
+        ...clickInfo.event.extendedProps,
+    });
+    setEventTitle(clickInfo.event.title);
+    setSelectedDate(clickInfo.event.start);
+    setAllDay(clickInfo.event.allDay);
+
+    if (!clickInfo.event.allDay) {
+        setStartTime(moment(clickInfo.event.start).format("HH:mm"));
+        setEndTime(moment(clickInfo.event.end).format("HH:mm"));
+    }
+    
     onOpen();
-    setSelectedDate(new Date(event.start));
-    setCurrentEvent(event);
-    setEventTitle(event.title);
-    setAllDay(event.allDay || false);
-    setStartTime(event.start ? moment(event.start).format("HH:mm") : '');
-    setEndTime(event.end ? moment(event.end).format("HH:mm") : '');
-  };
+};
 
   const saveEvent = () => {
-    const startDateTime = allDay ? moment(selectedDate).startOf('day').toDate() : moment(selectedDate).set({
-      hour: moment(startTime, "HH:mm").get('hour'),
-      minute: moment(startTime, "HH:mm").get('minute'),
-    }).toDate();
+    const startDateTime = allDay ? 
+        selectedDate : 
+        moment(selectedDate).set({
+            hours: moment(startTime, "HH:mm").hours(),
+            minutes: moment(startTime, "HH:mm").minutes(),
+        }).toDate();
 
-    const endDateTime = allDay ? moment(selectedDate).endOf('day').toDate() : moment(selectedDate).set({
-      hour: moment(endTime, "HH:mm").get('hour'),
-      minute: moment(endTime, "HH:mm").get('minute'),
-    }).toDate();
+    const endDateTime = allDay ? 
+        moment(selectedDate).add(1, 'day').toDate() :
+        moment(selectedDate).set({
+            hours: moment(endTime, "HH:mm").hours(),
+            minutes: moment(endTime, "HH:mm").minutes(),
+        }).toDate();
 
-    if (currentEvent) {
-      const updatedEvents = events.map((evt) =>
-        evt === currentEvent ? { ...evt, title: eventTitle, start: startDateTime, end: endDateTime, allDay } : evt
-      );
-      setEvents(updatedEvents);
-    } else {
-      const newEvent = {
+    let newEvent = {
+        id: currentEvent ? currentEvent.id : Date.now().toString(),
         title: eventTitle,
         start: startDateTime,
         end: endDateTime,
         allDay,
-        id: Math.random(),
-      };
-      setEvents([...events, newEvent]);
+    };
+
+    if (currentEvent) {
+        setEvents(events.map(event => event.id === currentEvent.id ? newEvent : event));
+    } else {
+        setEvents([...events, newEvent]);
     }
+
     onClose();
-  };
+};
 
   const deleteEvent = () => {
-    const filteredEvents = events.filter((evt) => evt !== currentEvent);
+    const filteredEvents = events.filter(event => currentEvent.id !== event.id);
     setEvents(filteredEvents);
     onClose();
   };
 
   const todaysEvents = events.filter((event) => moment().isSame(event.start, 'day'));
 
+
   return (
     <ChakraProvider>
-      <Box p={5}>
-        <VStack spacing={4}>
-          <Calendar
-            selectable
-            localizer={localizer}
-            events={events}
-            onSelectSlot={handleSelectSlot}
-            onSelectEvent={handleSelectEvent}
-            style={{ height: 500, width: '100%' }}
-          />
-          <Box borderWidth="1px" borderRadius="lg" p={4} width="100%" maxHeight="200px" overflow="auto">
-            <Text fontSize="xl" fontWeight="bold">Today's Events:</Text>
-            {todaysEvents.map((event, index) => (
-               <Box as="button" display="block" textAlign="left" width="100%" p={2} _hover={{ bg: "gray.100" }}
-               key={index}
-               onClick={() => handleSelectEvent(event)}
-             >
-             <Flex justify="space-between" align="center">
-                <Text fontWeight="semibold" textAlign="left">
-                  {event.title}
+      <Box className={colorMode} p={5} maxWidth="1000px" mx="auto">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={true}
+          events={events}
+          select={handleDateSelect}
+          eventClick={handleEventClick}
+          height="auto"
+        />
+          <Box borderWidth="1px" borderRadius="lg" p={4} mt={4} boxShadow="base">
+          <Text fontSize="xl" fontWeight="bold">Today's Events:</Text>
+          <VStack spacing={4}>
+          {todaysEvents.length > 0 ? (
+            todaysEvents.map((event) => (
+              <Flex key={event.id} justify="space-between" align="center" w="100%">
+                <Text>{event.title}</Text>
+                <Text fontSize="sm">
+                      {event.allDay ? 
+                          "All day" : 
+                           `${moment(event.start).format("HH:mm")} - ${moment(event.end).format("HH:mm")}`}
                 </Text>
-                <Text textAlign="right">
-                  {event.allDay ? "All Day" : `${moment(event.start).format("HH:mm")} to ${moment(event.end).format("HH:mm")}`}
-                </Text>
-              </Flex>
-              </Box>
-            ))}
-          </Box>
-        </VStack>
+             </Flex>
+            ))
+        ) : (
+            <Text>No events today.</Text>
+      )}
+          </VStack>
+        </Box>
+      
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>{currentEvent ? 'Edit Event' : 'Add New Event'}</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <FormControl>
+              <FormControl isRequired>
                 <FormLabel>Event Title</FormLabel>
                 <Input value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} placeholder="Enter event title" />
               </FormControl>
-              <Checkbox isChecked={allDay} onChange={(e) => setAllDay(e.target.checked)}>All Day Event</Checkbox>
+              <Checkbox isChecked={allDay} onChange={(e) => setAllDay(e.target.checked)} mt={4}>All Day Event</Checkbox>
               {!allDay && (
-                <>
-                  <FormControl>
+                <Flex mt={4}>
+                  <FormControl isRequired flex="1" mr={2}>
                     <FormLabel>Start Time</FormLabel>
                     <Input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
                   </FormControl>
-                  <FormControl>
+                  <FormControl isRequired flex="1">
                     <FormLabel>End Time</FormLabel>
                     <Input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
                   </FormControl>
-                </>
+                </Flex>
               )}
-              <SingleDatepicker
-                name="event-date"
-                date={selectedDate}
-                onDateChange={setSelectedDate}
-              />
             </ModalBody>
             <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={saveEvent}>
-                Save Event
-              </Button>
-              {currentEvent && (
-                <Button colorScheme="red" mr={3} onClick={deleteEvent}>
-                  Delete Event
-                </Button>
-              )}
-              <Button variant="ghost" onClick={onClose}>Cancel</Button>
+              <Button colorScheme="blue" mr={3} onClick={saveEvent}>Save</Button>
+              {currentEvent && <Button colorScheme="red" onClick={deleteEvent}>Delete</Button>}
+              <Button onClick={onClose}>Cancel</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
@@ -160,4 +170,4 @@ const MyCalendarWidget = () => {
   );
 };
 
-export default MyCalendarWidget;
+export default MyFullCalendar;
